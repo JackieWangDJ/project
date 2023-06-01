1056a38118beea5231e7bc3f6c24bf1a086b9c94
zhangyong <411815833@qq.com>
2023-06-01 17:33:57 +0800

    feat: 增加app首页

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
-  <div>
-    <h1 @click="handle">456</h1>
-  </div>
+  <el-row :gutter="20">
+    <el-col :span="8" :offset="0">
+      <el-image :src="imgUrl" fit="fill"></el-image>
+    </el-col>
+    <el-col :span="16" :offset="0">bbb</el-col>
+  </el-row>
 </template>
 <script setup lang="ts">
-import { reqLogin, reqUserInfo } from "@/api/user";
-import { loginResponseData, userResponseData } from "@/api/user/type";
-const handle = async () => {
-  const res: loginResponseData = await reqLogin({
-    username: "admin",
-    password: "111111",
-  });
-  const res1: userResponseData = await reqUserInfo();
-  console.log("res:", res.data);
-  console.log("res1:", res1.data);
-};
+import { ref } from "vue";
+const imgUrl = ref(
+  "https://img10.360buyimg.com/n2/s240x240_jfs/t1/210890/22/4728/163829/6163a590Eb7c6f4b5/6390526d49791cb9.jpg!q70.jpg"
+);
 </script>
 <style scoped lang="scss">
-div {
-  h1 {
-    color: $color;
+.el-image {
+  border: 3px solid black;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  justify-items: center;
+
+  :deep(.el-image__inner) {
+    width: 90%;
+    border-radius: 10px;
   }
 }
 </style>
